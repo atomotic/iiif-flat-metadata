d572561836734f824159c90e86f51cd9a76c5a0c
raffaele messuti <raffaele@atomotic.com>
2018-10-17 15:47:17 +0200

    add property: attribution, description

--- main.go
+++ main.go
@@ -11,9 +11,10 @@ import (
 )
 
 type Manifest struct {
-	Description string `json:"description"`
 	Id          string `json:"@id"`
 	Label       string `json:"label"`
+	Attribution string `json:"attribution"`
+	Description string `json:"description"`
 	Metadata    []struct {
 		Label string `json:"label"`
 		Value string `json:"value"`
@@ -38,6 +39,9 @@ func main() {
 
 	json.Unmarshal(m, &manifest)
 	metadata["@id"] = manifest.Id
+	metadata["Label"] = manifest.Label
+	metadata["Attribution"] = manifest.Attribution
+	metadata["Description"] = manifest.Description
 
 	for _, property := range manifest.Metadata {
 		if metadata[property.Label] != "" {
